ca50bd9b5ecc321cb0d42ca2dbbf49f182e6f7e0
toxpy <natoxpy@gmail.com>
2024-03-02 17:24:11 -0500

    feat: changes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "lua", "typescript", "javascript", "rust", "vue" },
-    callback = function()
-        vim.b.autoformat = true
-    end,
-})
+--  vim.api.nvim_create_autocmd({ "FileType" }, {
+--      pattern = { "lua", "typescript", "javascript", "rust", "vue" },
+--      callback = function()
+--          vim.b.autoformat = true
+--      end,
+--  })
+--
