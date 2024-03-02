e0b6fcadc61daf615358de0b86020e9873acb0f0
toxpy <natoxpy@gmail.com>
2024-03-02 11:11:30 -0500

    feat: LazyExtra additions

--- lua/plugins/no_names.lua
+++ lua/plugins/no_names.lua
@@ -1,5 +1,14 @@
 return {
     {
+        "s1n7ax/nvim-window-picker",
+        name = "window-picker",
+        event = "VeryLazy",
+        version = "2.*",
+        config = function()
+            require("window-picker").setup()
+        end,
+    },
+    {
         "hinell/lsp-timeout.nvim",
         dependencies = { "neovim/nvim-lspconfig" },
     },
